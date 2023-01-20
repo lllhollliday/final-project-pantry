2614fc3c1d03728cd7e3e5ca32603765d47193a1
Lucy <you@example.com>
2023-01-20 11:22:10 +0100

    search bar back end

--- backend/routes/recipesroute.js
+++ backend/routes/recipesroute.js
@@ -19,6 +19,7 @@ const router = express.Router()
 
 router.get("/", async (req, res) => {
   const { q, cuisineType, mealType, health } = req.query
+  console.log("this is a text", req.query)
 
   try {
     const found = await recipeCollection.findOne({
@@ -27,22 +28,39 @@ router.get("/", async (req, res) => {
       mealType,
       health,
     })
+    console.log("Found is", found)
     if (found) {
       res.json(found)
     } else {
-        console.log(q, cuisineType, mealType, health)
+      console.log(q, cuisineType, mealType, health)
+   
       const { data } = await axios.get(
-        `https://api.edamam.com/api/recipes/v2?type=public&q=${q}&cuisineType=${cuisineType}&mealType=${mealType}&health=${health}&app_id=8d589cb8&app_key=4c1d224144b3fa7000b2320750876ef8&imageSize=LARGE`
+  
+        `https://api.edamam.com/api/recipes/v2?type=public${
+          q ? "&q=" + q : ""
+        }${cuisineType ? "&cuisineType=" + cuisineType : ""}${
+          mealType ? "&mealType=" + mealType : ""
+        }${
+          health ? "&health=" + health : ""
+        }&app_id=8d589cb8&app_key=4c1d224144b3fa7000b2320750876ef8&imageSize=LARGE`
       )
-      console.log(data)
-      const recipe = new recipeCollection({
-        q, cuisineType, mealType, health,
-        recipes: data.hits,
-      })
-      await recipe.save()
+      console.log("this is res from data:", data)
+      let recipe = []
+      if (data.count > 0) {
+        recipe = new recipeCollection({
+          q,
+          cuisineType,
+          mealType,
+          health,
+          recipes: data.hits,
+        })
+        await recipe.save()
+      }
+
       res.json(recipe)
     }
   } catch (error) {
+    console.log("Error searching for recipes", error.message)
     res.status(500).json({ message: "Error searching for recipes" })
   }
 })
