76e712753857c7057397d17c5d281d6a28929dc2
o.marci@gmx.de <o.marci@gmx.de>
2023-01-24 11:46:33 +0100

    carousel aligned

--- frontend/src/components/CarouselLanding.js
+++ frontend/src/components/CarouselLanding.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
+import { Row, Col } from 'react-bootstrap'
 import image1 from '../media/images/image1.jpg';
 import image2 from '../media/images/image2.jpg';
 import image3 from '../media/images/image3.jpg';
@@ -10,42 +11,68 @@ const CarouselLanding = () => {
     <>
     <Carousel>
       <Carousel.Item>
-        <img
-          className={styles["slider"]}
-          src={image1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Recipe of the Day 1</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className={styles["slider"]}
-          src={image2}
-          alt="Second slide"
-        />
 
-        <Carousel.Caption>
-          <h3>Recipe of the Day 2</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className={styles["slider"]}
-          src={image3}
-          alt="Third slide"
-        />
+        <Row>
 
-        <Carousel.Caption>
-          <h3>Recipe of the Day 3</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+          <Col className={styles["container-img-carousel"]}>
+            <img
+            className={styles["slider"]}
+            src={image1}
+            alt="Second slide"
+            />
+          </Col>
+
+          <Col>
+            
+              <h3>Recipe of the day </h3>
+              <h4>Pasta with Tomato sauce.</h4>
+              <p>Simple but very tasty</p>
+                          
+          </Col>
+
+        </Row>
       </Carousel.Item>
+
+      <Carousel.Item>
+        <Row>
+
+          <Col className={styles["container-img-carousel"]}>
+            <img
+            className={styles["slider"]}
+            src={image2}
+            alt="Second slide"
+            />
+          </Col>
+
+          <Col>
+            
+              <h3>Do you still have chicken in the fridge?</h3>
+              <p>Quick recipes for lunch time</p>
+            
+          </Col>
+
+        </Row>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <Row>
+
+          <Col className={styles["container-img-carousel"]}>
+            <img
+            className={styles["slider"]}
+            src={image3}
+            alt="Photo of a salad"
+            />
+          </Col>
+
+          <Col>
+              <h3>Only brocoli in the pantry? </h3>
+              <p>We have the recipe that fits to you</p>
+          </Col>
+
+        </Row>
+      </Carousel.Item>
+
     </Carousel>
     </>
   );
