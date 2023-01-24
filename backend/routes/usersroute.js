19dae95e76e5b2ab257d870e54338fb19094e918
Jason Lee <jasonw_lee@hotmail.com>
2023-01-24 12:06:07 +0100

    authenticateToken-incomplete

--- backend/routes/usersroute.js
+++ backend/routes/usersroute.js
@@ -25,12 +25,13 @@ export default route */
 
 import express from "express";
 import { createUser, deleteUser, getAllUsers, getSingleUser, loginUser, updateUser } from "../controllers/userscontrollers.js";
+import authenticateToken from "../middleware/authenticateToken.js";
 
 const router = express.Router();
 
 // Router Get "/users"
-router.get('/', getAllUsers);
-router.get('/:id', getSingleUser);
+router.get('/', getAllUsers, authenticateToken);
+router.get('/:id', getSingleUser, authenticateToken);
 // Router Post "/users"
 router.post('/register', createUser)
 router.post('/login', loginUser);
