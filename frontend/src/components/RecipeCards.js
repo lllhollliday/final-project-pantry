42047d35e6e3554571bab80539bc1f016d99b0b0
Lucy <you@example.com>
2023-01-27 18:33:49 +0100

    updated a few css bits

--- frontend/src/components/RecipeCards.js
+++ frontend/src/components/RecipeCards.js
@@ -1,4 +1,3 @@
-
 import axios from "axios"
 import { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
@@ -10,24 +9,25 @@ import { globalContext } from "../context/globalContext"
 export default function RecipeCards() {
   const { recipes, user, setUser } = useContext(globalContext)
 
-  const handleLike = async(item) => {
-    console.log(item);
+  const handleLike = async (item) => {
+    console.log(item)
     try {
-      const res = await axios.put(`http://localhost:8000/users/favourites`, {item, id: user._id})
+      const res = await axios.put(`http://localhost:8000/users/favourites`, {
+        item,
+        id: user._id,
+      })
       // setFavourite([...favourites, item])
 
       setUser({
         ...user,
-        favourites: res.data.favourites
+        favourites: res.data.favourites,
       })
 
-      console.log(res);
-    } catch(err){
-      console.log(err);
+      console.log(res)
+    } catch (err) {
+      console.log(err)
     }
   }
-
-
 
   return (
     <FlexWrap>
@@ -35,25 +35,27 @@ export default function RecipeCards() {
         {recipes?.map((item) => {
           return (
             <StyledLink to={"/recipe/" + item.label} state={item}>
-              <div>
-                <CardImg key={item._id}>
-                  <img src={item.image} alt={item.label} />
-             
-                </CardImg>{" "}
-                <TitleTextWrapper>
-                  <CardTitle>
-                    {item.label}
-                    <p>
-                      {item.totalTime > 0
-                        ? `Preparation time: ${item.totalTime} mins`
-                        : ""}
-                    </p>
-                  </CardTitle>     
-                  
-                  <button onClick={() => handleLike(item)}>{user?.favourites?.findIndex(fav => fav.label === item.label) > -1 ? "unlike pic" : "like pic"}</button>
-                  
-                </TitleTextWrapper>
-              </div>
+              <CardImg key={item._id}>
+                <img src={item.image} alt={item.label} />
+              </CardImg>{" "}
+              <TitleTextWrapper>
+                <CardTitle>
+                  {item.label}
+                  <p>
+                    {item.totalTime > 0
+                      ? `Preparation time: ${item.totalTime} mins`
+                      : ""}
+                  </p>
+                </CardTitle>
+
+                <button onClick={() => handleLike(item)}>
+                  {user?.favourites?.findIndex(
+                    (fav) => fav.label === item.label
+                  ) > -1
+                    ? "unlike pic"
+                    : "like pic"}
+                </button>
+              </TitleTextWrapper>
             </StyledLink>
           )
         })}
@@ -63,19 +65,20 @@ export default function RecipeCards() {
 }
 
 const FlexWrap = styled.div`
-  //margin-top: 10rem;
   display: flex;
   justify-content: center;
+  width: 100vw;
 `
 
 const Wrapper = styled.div`
   justify-items: center;
-  width: 90%;
+  width: 70vw;
   margin: 4rem 0rem;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-column-gap: 20px;
   grid-row-gap: 5rem;
+
 
   @media (max-width: 480px) {
     width: 90vw;
@@ -88,10 +91,10 @@ const Wrapper = styled.div`
 
 const CardImg = styled.div`
   width: 100%;
-  min-height: 12rem;
-  max-height: 15rem;
+
+  max-height: 18rem;
   border-radius: 2rem;
-  //border: 1px solid #ffb8038e;
+
   -webkit-box-shadow: -7px 3px 31px -15px rgba(133, 133, 133, 0.81);
   box-shadow: -7px 3px 31px -15px rgba(133, 133, 133, 0.81);
   overflow: hidden;
@@ -130,7 +133,6 @@ const CardTitle = styled.div`
 
   p {
     font-size: 13px;
-    
   }
 `
 
