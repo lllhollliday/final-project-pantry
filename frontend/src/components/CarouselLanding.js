cab18c3cfb4e6ab7868d1029f9a80f2ffa089aed
Jason Lee <jasonw_lee@hotmail.com>
2023-01-27 09:27:20 +0100

    mediaqueries

--- frontend/src/components/CarouselLanding.js
+++ frontend/src/components/CarouselLanding.js
@@ -1,15 +1,16 @@
 import  {useContext} from "react";
 import { globalContext } from "../context/globalContext";
-import {Carousel, CarouselItem} from 'react-bootstrap';
+import {Carousel, CarouselItem, CarouselControl} from 'react-bootstrap';
 // import { Row, Col } from 'react-bootstrap';
-import styles from './CarouselLanding.module.css'
+// import styles from './CarouselLanding.module.css';
+import styled from 'styled-components';
 
 
 const CarouselLanding = () => {
     const {sliderItems} = useContext(globalContext);
 
     return(
-        <Carousel>
+        <Carousel controls={true} indicators={false}>
             {sliderItems.map((item, index) => {
                 return (
                 <CarouselItem 
@@ -18,12 +19,13 @@ const CarouselLanding = () => {
                         width:"100vw", 
                         height:"400px", 
                         textAlign:"center", 
-                        paddingTop:"50px"
+                        paddingTop:"50px", 
+                        borderRadius: "2rem"
                         }}>
-                    <img 
+                    <StyledImg 
                         src={item.image} 
                         alt={item.label} 
-                        style={{width:"35vw", height:"300px", borderRadius: "25px"}} 
+                        // style={{width:"35vw", height:"300px", borderRadius: "25px"}} 
                         interval={100}
                         />
                     <h3 style={{
@@ -41,3 +43,14 @@ const CarouselLanding = () => {
 
 export default CarouselLanding;
 
+const StyledImg = styled.img`
+  width: 35vw;
+  height: 300px;
+  border-radius: 2rem;
+
+  @media (max-width: 768px) {
+    width: 80%;
+    height: auto;
+    border-radius: 0;
+  }
+`;
