d21a6922a13cc6451a8b4122b46f987259c06caf
Lucy <you@example.com>
2023-01-29 17:59:38 +0100

    finished-ish fav button

--- frontend/src/components/RecipeCards.js
+++ frontend/src/components/RecipeCards.js
@@ -3,6 +3,35 @@ import { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import { globalContext } from "../context/globalContext"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons"
+import { faHeart as regHeart } from "@fortawesome/free-regular-svg-icons"
+
+const like = (
+  <FontAwesomeIcon
+    icon={solidHeart}
+    style={{
+      color: "#ffb803",
+      position: "absolute",
+      top: "6px",
+      right: "5px",
+    }}
+  />
+)
+
+const unlike = (
+  <FontAwesomeIcon
+    icon={regHeart}
+    style={{
+      color: " #ffb803",
+      position: "absolute",
+      top: "6px",
+      right: "5px",
+
+ 
+    }}
+  />
+)
 
 // home page to render the top random recipes as the first cards
 
@@ -34,29 +63,30 @@ export default function RecipeCards() {
       <Wrapper>
         {recipes?.map((item) => {
           return (
-            <StyledLink to={"/recipe/" + item.label} state={item}>
-              <CardImg key={item._id}>
-                <img src={item.image} alt={item.label} />
-              </CardImg>{" "}
-              <TitleTextWrapper>
-                <CardTitle>
-                  {item.label}
-                  <p>
-                    {item.totalTime > 0
-                      ? `Preparation time: ${item.totalTime} mins`
-                      : ""}
-                  </p>
-                </CardTitle>
-
-                <button onClick={() => handleLike(item)}>
-                  {user?.favourites?.findIndex(
-                    (fav) => fav.label === item.label
-                  ) > -1
-                    ? "unlike pic"
-                    : "like pic"}
-                </button>
-              </TitleTextWrapper>
-            </StyledLink>
+            <div style={{ position: "relative" }}>
+              <StyledLink to={"/recipe/" + item.label} state={item}>
+                <CardImg key={item._id}>
+                  <img src={item.image} alt={item.label} />
+                </CardImg>{" "}
+                <TitleTextWrapper>
+                  <CardTitle>
+                    {item.label}
+                    <p>
+                      {item.totalTime > 0
+                        ? `Preparation time: ${item.totalTime} mins`
+                        : ""}
+                    </p>
+                  </CardTitle>
+                </TitleTextWrapper>
+              </StyledLink>
+              <StyledButton onClick={() => handleLike(item)}>
+                {user?.favourites?.findIndex(
+                  (fav) => fav.label === item.label
+                ) > -1
+                  ? like
+                  : unlike}
+              </StyledButton>
+            </div>
           )
         })}
       </Wrapper>
@@ -72,13 +102,12 @@ const FlexWrap = styled.div`
 
 const Wrapper = styled.div`
   justify-items: center;
-  width: 70vw;
+  width: 50vw;
   margin: 4rem 0rem;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-column-gap: 20px;
   grid-row-gap: 5rem;
-
 
   @media (max-width: 480px) {
     width: 90vw;
@@ -90,14 +119,21 @@ const Wrapper = styled.div`
 `
 
 const CardImg = styled.div`
+  border: 1px solid green;
   width: 100%;
 
-  max-height: 18rem;
+  max-height: 20rem;
   border-radius: 2rem;
 
   -webkit-box-shadow: -7px 3px 31px -15px rgba(133, 133, 133, 0.81);
   box-shadow: -7px 3px 31px -15px rgba(133, 133, 133, 0.81);
   overflow: hidden;
+  transition: ease 0.1s;
+
+/*   :hover {
+    border: 2px solid green;
+    transition: 0.2s ease;
+  } */
 
   @media (max-width: 600px) {
     min-height: 10rem;
@@ -109,7 +145,8 @@ const CardImg = styled.div`
   }
 
   img {
-    object-fit: contain;
+    object-fit: cover;
+
     //object-position: 10px;
     width: 150%;
     height: 100%;
@@ -140,5 +177,19 @@ const StyledLink = styled(Link)`
   text-decoration: none;
   &:hover {
     font-weight: 380;
+  }
+`
+const StyledButton = styled.button`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  border: 1px solid green;
+  border-radius: 40%;
+  width: 28px;
+  height: 30px;
+  background-color: #3e654479;
+
+  :hover{
+    background-color:  #3e6544ac;
   }
 `
