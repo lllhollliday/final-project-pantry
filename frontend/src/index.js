3b131c5882da32f14c20cd16bb75bce6ff8178fc
Lucy <you@example.com>
2023-02-14 13:30:41 +0100

    Browser to Hashrouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import GlobalContainer from "./context/globalContainer.js"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <GlobalContainer>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </GlobalContainer>
 )
